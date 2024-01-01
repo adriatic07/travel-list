794bae439e1ef3807131c5b225390a7c0dd966b0
Aniket Jain <jaina290@gmail.com>
2024-01-01 23:13:16 +0530

    Intial Commit

---
+++ src/components/App.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+import Logo from "./Logo";
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+// const initialItems = [
+//   { id: 1, description: "Passports", quantity: 2, packed: false },
+//   { id: 2, description: "Socks", quantity: 12, packed: true },
+//   { id: 3, description: "Charger", quantity: 1, packed: false },
+//   { id: 4, description: "Cap", quantity: 1, packed: false },
+//   { id: 5, description: "Passports", quantity: 2, packed: false },
+//   { id: 6, description: "Socks", quantity: 12, packed: true },
+//   { id: 7, description: "Charger", quantity: 1, packed: false },
+//   { id: 8, description: "Cap", quantity: 1, packed: false },
+// ];
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+  function handleClearItem() {
+    const confirm = window.confirm(
+      "Are you sure you want delete to all items from the list?"
+    );
+
+    if (confirm) setItems([]);
+  }
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+        onClearItem={handleClearItem}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
